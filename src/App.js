38c07f379611b981f43a19336a131d32d0e3dc4c
sam-adroit <samuelbankole779@gmail.com>
2021-11-06 00:46:22 +0100

    Update menu-items to navigate to other pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.components';
+
+const HatsPage = props => {
+  console.log(props);
+  return(
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='shop/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+// import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`menu-item ${size}`}>
-        <div 
-            className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'> SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    let navigate = useNavigate();
+    return (
+        <div className={`menu-item ${size}`} onClick={() => navigate(`${linkUrl}`)}>
+            <div 
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'> SHOP NOW</span>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default MenuItem;  
+// export default withRouter(MenuItem);  
