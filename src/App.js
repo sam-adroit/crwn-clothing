4da3186c4673262213cde67bac3e49c3d0414a0e
sam-adroit <samuelbankole779@gmail.com>
2021-11-06 03:14:29 +0100

    Header component added to all pages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './page/homepage/homepage.components';
 import ShopPage from './page/shop/shop.component';
+import Header from './component/header/header.component';
     
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='/' element={ <HomePage /> } />
         <Route path='shop' element={<ShopPage />} />
